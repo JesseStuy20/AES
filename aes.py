959e6933e3008881bbcf060b744770aaeb8c8b0c
Jesse Xie <72952857+JesseStuy20@users.noreply.github.com>
2021-12-07 15:32:34 -0500

    Update aes.py

--- aes.py
+++ aes.py
@@ -6,7 +6,14 @@ key = open(keyfile,"rb").read()
 inp = open(inpfile,"rb").read()
 debug = False
 
+if(len(key) != 16):
+  print("the key length must be 16 characters")
+
 if(debug):
   print("mode:"+mode)
   print("key: "+key)
   print("inp: "+inp)
+
+def blocks(inp):
+  blocks = []
+
